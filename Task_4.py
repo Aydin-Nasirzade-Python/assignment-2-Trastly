7b64206ed94c2bb317fc5d2318601e9293fa7667
Huseyn Alizade <161739380+Trastly@users.noreply.github.com>
2024-04-07 20:47:15 +0400

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -2,24 +2,36 @@
 
 def main():
     def determine_chinese_zodiac(year):
-      zodiac_animals = {0: "Monkey",1: "Rooster",2: "Dog",3: "Pig",4: "Rat",5: "Ox",6: "Tiger",7: "Hare",8: "Dragon",9: "Snake",10: "Horse",11: "Sheep"}
-      if year >= 0:
-          animal_index = year % 12
-          return zodiac_animals[animal_index]
-      else:
-          return None
+        zodiac_animals = {
+            0: "Dragon",
+            1: "Snake",
+            2: "Horse",
+            3: "Sheep",
+            4: "Monkey",
+            5: "Rooster",
+            6: "Dog",
+            7: "Pig",
+            8: "Rat",
+            9: "Ox",
+            10: "Tiger",
+            11: "Hare"}
+        if year >= 0:
+            animal_index = (year - 2000) % 12
+            return zodiac_animals[animal_index]
+        else:
+            return None
 
-  while True:
-      year = int(input("Enter the year [ex. 2021]: "))
-      if year >= 0:
-          zodiac_animal = determine_chinese_zodiac(year)
-          if zodiac_animal:
-              print(f"{year} is the year of the {zodiac_animal}")
-              break
-          else:
-              print("Invalid year!")
-      else:
-        print("Invalid year!")
+    while True:
+        year = int(input("Enter the year [ex. 2021]: "))
+        if year >= 0:
+            zodiac_animal = determine_chinese_zodiac(year)
+            if zodiac_animal:
+                print(f"{year} is the year of the {zodiac_animal}")
+                break
+            else:
+                print("Invalid year!")
+        else:
+            print("Invalid year!")
 
   pass
 

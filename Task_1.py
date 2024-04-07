e0fadcba4820817d97260006360b83b3687bbeaa
Huseyn Alizade <161739380+Trastly@users.noreply.github.com>
2024-04-07 19:51:09 +0400

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,9 +1,23 @@
 #import libraries here
 
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
+  def check_letter(letter):
+      vowels = ['a', 'e', 'i', 'o', 'u']
+      if letter.lower() in vowels:
+          print("Entered alphabet is a vowel!")
+      elif letter.lower() == 'y':
+          print("Sometimes it is a vowel, and sometimes it is a consonant!")
+      else:
+          print("Entered alphabet is a consonant!")
+
+  while True:
+      user_input = input("Enter a letter of the alphabet: ")
+      if len(user_input) == 1 and user_input.isalpha():
+          check_letter(user_input)
+          break
+      else:
+          print("Please enter a single letter of the alphabet!")
+
   pass
 
 if __name__ == "__main__":

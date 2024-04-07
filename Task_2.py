46557afd46bbea59928a5ea3733c2ec84bfb1fa7
Huseyn Alizade <161739380+Trastly@users.noreply.github.com>
2024-04-07 21:58:35 +0400

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -1,9 +1,27 @@
 #import libraries here
 
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
+  def determine_season(month, day):
+      if (month == "March" and day >= 20) or (month == "April") or (month == "May") or (month == "June" and day < 21):
+          return "Spring"
+      elif (month == "June" and day >= 21) or (month == "July") or (month == "August") or (month == "September" and day < 22):
+          return "Summer"
+      elif (month == "September" and day >= 22) or (month == "October") or (month == "November") or (month == "December" and day < 21):
+          return "Fall"
+      else:
+          return "Winter"
+
+  while True:
+      month = input("Enter name of the month [ex. June]: ")
+      day = int(input("Enter the day [ex. 5]: "))
+      if month.capitalize() in ["January", "February", "March", "April", "May", "June",
+                           "July", "August", "September", "October", "November", "December"]:
+          if 1 <= day <= 31:
+              season = determine_season(month.capitalize(), day)
+              print(f"{month} {day} is in {season}")
+              break
+      print("Invalid input!")
+
   pass
 
 if __name__ == "__main__":

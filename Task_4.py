7205d78e84bd10b78dbeb630f15597b0adf11ff7
Huseyn Alizade <161739380+Trastly@users.noreply.github.com>
2024-04-07 23:02:05 +0400

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -1,34 +1,19 @@
 #import libraries here
 
 def main():
-    def determine_zodiac_animal(year):
-        zodiac_animals = {
-            2000: "Dragon",
-            2001: "Snake",
-            2002: "Horse",
-            2003: "Sheep",
-            2004: "Monkey",
-            2005: "Rooster",
-            2006: "Dog",
-            2007: "Pig",
-            2008: "Rat",
-            2009: "Ox",
-            2010: "Tiger",
-            2011: "Hare"
-        }
-        return zodiac_animals.get(year)
+    chinese_zodiac = {2000: "Dragon",2001: "Snake",2002: "Horse",2003: "Sheep",2004: "Monkey",2005: "Rooster",2006: "Dog",2007: "Pig",2008: "Rat",2009: "Ox",2010: "Tiger",2011: "Hare"}
 
-    while True:
-        year = int(input("Enter the year [ex. 2021]: "))
-        if year >= 0:
-            zodiac_animal = determine_zodiac_animal(year)
-            if zodiac_animal:
-                  print(f"{year} is the year of the {zodiac_animal}")
-                break
-            else:
-                print("Invalid year!")
-        else:
-            print("Invalid year!")
+    year = int(input("Enter the year [ex. 2021]: "))
+
+    if year < 0:
+        print("Invalid year!")
+    else:
+        zodiac_year = year % 12
+        animal = chinese_zodiac.get(zodiac_year + 2000)  # Add 2000 to align with the start of the cycle
+
+
+        print(f"{year} is the year of the {animal}")
+
 
 
   pass

e85416766c649e1dacd2283c7e724a60197be3e8
Huseyn Alizade <161739380+Trastly@users.noreply.github.com>
2024-04-07 22:23:46 +0400

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -1,30 +1,6 @@
 #import libraries here
 
 def main():
-  def determine_color(wavelength):
-      if 380 <= wavelength < 450:
-        return "Violet"
-      elif 450 <= wavelength < 495:
-          return "Blue"
-      elif 495 <= wavelength < 570:
-          return "Green"
-      elif 570 <= wavelength < 590:
-          return "Yellow"
-      elif 590 <= wavelength < 620:
-          return "Orange"
-      elif 620 <= wavelength <= 750:
-          return "Red"
-      else:
-          return None
-
-  while True:
-      wavelength = int(input("Enter the wavelength in nm: "))
-      if 380 <= wavelength <= 750:
-          color = determine_color(wavelength)
-          if color:
-              print("The relevant color is", color)
-              break
-      print("Invalid input!")
 
   pass
 
